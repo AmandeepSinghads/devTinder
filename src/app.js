ca68fdd8a457fe793961ac95e96b0b8f0969716a
Amandeep Singh <ads.singh05@gmail.com>
2025-12-02 22:23:01 +0530

    hello

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 const express = require("express");
-
 const app = express();
 
-app.listen(3000);
+app.use("/hello", (req, res) => {
+  res.send("Hello world!");
+});
+app.use("/", (req, res) => {
+  res.send("Home page");
+});
+app.listen(3000, () => {
+  console.log("Server is running");
+});
